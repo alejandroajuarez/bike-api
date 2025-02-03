2b8d3920e86a6804977ca7e667981f93967674bd
alejandrojuarez117 <aljuarez117@gmail.com>
2025-02-02 21:44:23 -0700

    finished bikes controller

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -4,4 +4,39 @@ class BikesController < ApplicationController
     @bikes = Bike.all
     render :index
   end
+
+  def show
+    id_find = params[:id]
+    @bike = Bike.find_by(id: id_find)
+  end
+
+  def create
+    @bike = Bike.new(
+      name:         params[:name],
+      brand:        params[:brand],
+      model_year:   params[:model_year],
+      price:        params[:price],
+      description:  params[:description],
+      category:     params[:category]
+    )
+    @bike.save
+    render :show
+  end
+
+  def update
+    @bike = Bike.find_by(id: params[:id])
+    @bike.name          = params[:name]
+    @bike.brand         = params[:brand]
+    @bike.model_year    = params[:model_year]
+    @bike.price         = params[:price]
+    @bike.description   = params[:description]
+    @bike.category      = params[:category]
+    render :show
+  end
+
+  def destroy
+    bike = Bike.find_by(id: params[:id])
+    bike.destroy
+    render json: { message: "Bike entry has been destroyed!" }
+  end
 end
