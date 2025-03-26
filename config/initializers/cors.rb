ef767c1c53f19c46cd33b30b9177d0e6fdb84229
alejandrojuarez117 <aljuarez117@gmail.com>
2025-03-25 18:00:47 -0600

    added frontend url to connect with backend on render

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'  # Replace '*' with specific domains like 'example.com' for security
-
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    origins "example.com", "localhost:5173", "bike-frontend-i9kq.onrender.com"
+    resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
